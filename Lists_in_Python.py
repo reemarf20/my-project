2f6509cf3287d354307235f4a406fd90f15be805
reemarf.20 <reemarizan08@gmail.com>
2023-10-15 20:36:45 +0530

    lets check if an item exists (python lists)

--- Lists_in_Python.py
+++ Lists_in_Python.py
@@ -3,3 +3,11 @@ print(fruits[1])
 
 #Lets add 'Orange' to the list
 fruits.append("orange")
+
+#Lets check if an item is there in the list
+user_input = input("Enter the name of a fruit: ")
+
+if user_input in fruits:
+    print("Yes, " + user_input + " is in the list.")
+else:
+    print("No, " + user_input + " is not in the list.")
